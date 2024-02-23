dffac1f8db963dfa54b298baf773b61b27a05e1c
Rajashekarksreddy <rajksreddy@gmail.com>
2024-02-23 22:22:04 +0530

    added-route/path

--- routes/route.js
+++ routes/route.js
@@ -2,8 +2,8 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/test-me', function(req,res){
-    res.send('my first ever api')
+router.get('/test-route', function(req,res){
+    res.send('my first ever api and routes ')
 })
 
 module.exports = router
