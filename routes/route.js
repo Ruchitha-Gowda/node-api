52000538b37139a2003f45d601e0d70a50e3e95c
Rajashekarksreddy <rajksreddy@gmail.com>
2024-02-24 20:01:36 +0530

    added-handler

--- routes/route.js
+++ routes/route.js
@@ -6,4 +6,8 @@ router.get('/test-route', function(req,res){
     res.send('my first ever api and routes ')
 })
 
+router.get('/test-me', function(req,res){
+    res.send('my first ever api and routes ')
+})
+
 module.exports = router
